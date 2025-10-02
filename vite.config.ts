a85e334496d88ebe3e402296719b94fdb9844a30
tmg_utsav <122513109+Ecchisen@users.noreply.github.com>
2025-10-02 07:49:34 +0000

    lil tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/scholars_site/",  
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
+    },
+  },
+  base: "/scholars_site/",
 })
 
